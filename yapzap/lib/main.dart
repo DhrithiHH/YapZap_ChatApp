02fc535bfeaeca0c26f7589d3fbe7ca44415c2f6
charanrajb28 <charanrajb282004@gmail.com>
2024-11-23 08:28:16 +0530

    added screen

--- yapzap/lib/main.dart
+++ yapzap/lib/main.dart
@@ -1,29 +1,60 @@
+// import 'package:flutter/material.dart';
+// import 'package:lottie/lottie.dart'; // Import the Lottie package
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Lottie Animation Example'), // Title of your app
+//         ),
+//         body: Center(
+//           child: Lottie.asset(
+//             'assets/animations/alien_angry.json', // Path to your animation JSON file
+//             width: 200, // Set width of the animation
+//             height: 200, // Set height of the animation
+//             fit: BoxFit.contain, // Control the animation's fit
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart'; // Import the Lottie package
+import '../screens/navigation_screen.dart';
+import 'screens/login_screen_demo.dart';
+import 'screens/register_screen_demo.dart';
+import 'screens/home_page_demo.dart';
+import 'screens/chat_screen.dart';
+import 'screens/forgot_password_screen_demo.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Lottie Animation Example'), // Title of your app
-        ),
-        body: Center(
-          child: Lottie.asset(
-            'assets/animations/animation.json', // Path to your animation JSON file
-            width: 200, // Set width of the animation
-            height: 200, // Set height of the animation
-            fit: BoxFit.contain, // Control the animation's fit
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Navigation Test',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => NavigationTestScreen(),
+        '/login': (context) => LoginScreen(),
+        '/register': (context) => RegisterScreen(),
+        '/homepage': (context) => HomePage(),
+        '/chatscreen': (context) => ChatScreen(),
+        '/forgotpassword': (context) => ForgotPasswordScreen(),
+      },
     );
   }
 }
+
---
+++ yapzap/lib/screens/navigation_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class NavigationTestScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Navigation Test Screen'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(16.0),
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/login');
+            },
+            child: Text('Go to Login Screen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/register');
+            },
+            child: Text('Go to Register Screen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/homepage');
+            },
+            child: Text('Go to Home Page'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/chatscreen');
+            },
+            child: Text('Go to Chat Screen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/forgotpassword');
+            },
+            child: Text('Go to Forgot Password Screen'),
+          ),
+        ],
+      ),
+    );
+  }
+}
